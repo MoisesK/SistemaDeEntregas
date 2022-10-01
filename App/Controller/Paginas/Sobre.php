e3b38bcffa582db6921e7fa0c6c4477f5ab41d58
MoisesK <moises979@gmail.com>
2022-09-30 21:06:50 -0300

    feat: Controller e View da Página Sobre.

---
+++ App/Controller/Paginas/Sobre.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller\Paginas;
+
+use App\Utilitarios\View;
+
+class Sobre extends Page{
+
+//Metodo qu retornar o conteúdo(View) da PÁGINA SOBRE;
+  public static function getSobre() :string
+  {
+    //Retorna a View da Home.
+    $conteudo = View::render('Paginas/sobre',[
+      "PageName" => "Sobre o Projeto",
+      "Descrição" => "Seja bem vindo ao Entregas 2000,
+      o Sistema de entregas perfeito para seu negócio."
+    ]);
+
+    //Retorna a View da Página
+    return parent::getPage("Sobre > E2000", $conteudo);
+  }
+
+}
+
+
+
+ ?>
---
+++ Resources/View/Paginas/Sobre.php
@@ -0,0 +1,3 @@
+<h1>{{PageName}} </h1>
+<hr>
+<p>{{Descrição}}</p>
