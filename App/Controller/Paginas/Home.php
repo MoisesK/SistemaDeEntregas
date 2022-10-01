7810600c6bce5309278e33994f7d0513439cec4e
MoisesK <moises979@gmail.com>
2022-09-30 21:08:17 -0300

    feat: Nav-Bar funcional.

--- App/Controller/Paginas/Home.php
+++ App/Controller/Paginas/Home.php
@@ -6,18 +6,16 @@ use App\Utilitarios\View;
 
 class Home extends Page{
 
-//Metodo qu retornar o conteúdo(View) da Home;
+//Metodo qu retornar o conteúdo(View) da PÁGINA HOME;
   public static function getHome() :string
   {
     //Retorna a View da Home.
     $conteudo = View::render('Paginas/Home',[
       "HomeName" => "Sistema de Entregas",
-      "DescriçãoHome" => "Seja bem vindo ao Entregas 2000,
-      o Sistema de entregas perfeito para seu negócio."
     ]);
 
     //Retorna a View da Página
-    return parent::getPage("Home - Entregas 2000", $conteudo);
+    return parent::getPage("Home > E2000", $conteudo);
   }
 
 }
--- Resources/View/Paginas/Header.php
+++ Resources/View/Paginas/Header.php
@@ -3,8 +3,8 @@
 <h1>ENTREGAS 2000</h1>
 <p>MODEL - VIEW - CONTROLLER</p>
 <ul class="nav nav-pills">
-    <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Home</a></li>
-    <li class="nav-item"><a href="##" class="nav-link">Menu1</a></li>
+    <li class="nav-item"><a href="{{URL}}" class="nav-link active">Home</a></li>
+    <li class="nav-item"><a href="{{URL}}/sobre" class="nav-link">SOBRE</a></li>
     <li class="nav-item"><a href="#" class="nav-link">Menu2</a></li>
     <li class="nav-item"><a href="#" class="nav-link">Menu3</a></li>
     <li class="nav-item"><a href="#" class="nav-link">Menu4</a></li>
