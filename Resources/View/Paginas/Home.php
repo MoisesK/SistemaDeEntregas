792bb424ba277ec3f817250a4b79ff98023b66fa
MoisesK <moises979@gmail.com>
2022-10-03 20:11:18 -0300

    update: Melhoria exibição de informações na home.

--- Resources/View/Paginas/Home.php
+++ Resources/View/Paginas/Home.php
@@ -3,11 +3,29 @@
 
 <section id="listaEntregas">
     <div class="card text-dark mb-3">
-        <h5 class="card-header">Entregador: Moisés Prazo Da entrega -</h5>
+        <h5 class="card-header text-center">Entregador - Moisés </h5>
         <div class="card-body">
-            Titulo da Entrega - <br>
-            Descrição da Entrega - <br>
-            Status da Entrega<br>
+            <div class="table-responsive">
+                <table class="table ">
+                    <thead>
+                        <tr>
+                            <th scope="col">Prazo Da entrega</th>
+                            <th scope="col">Titulo da Entrega</th>
+                            <th scope="col">Descrição da Entrega</th>
+                            <th scope="col">Status da Entrega</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr class="">
+                            <td>Prazo Da entrega 1</td>
+                            <td>Titulo da Entrega 1</td>
+                            <td>Descrição da Entrega 1</td>
+                            <td>Status da Entrega 1</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            
         </div>
     </div>
 
