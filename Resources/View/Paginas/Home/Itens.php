a9e126249bdd1903c2325e6e9fe2c5c7c1e3bbc7
MoisesK <moises979@gmail.com>
2022-10-04 20:24:49 -0300

    feat: Exibição de itens

---
+++ Resources/View/Paginas/Home/Itens.php
@@ -0,0 +1,10 @@
+<tbody>
+    <tr class="">
+        <td>{{PrazoEntrega}}</td>
+        <td>{{TituloEntrega}}</td>
+        <td>{{DescriçãoEntrega}}</td>
+        <td>{{LocalEntrega}}</td>
+        <td>{{StatusEntrega}}</td>
+        
+    </tr>
+</tbody>
