9c5b04a66a60855f968f3a7b8283235b085b758c
MoisesK <moises979@gmail.com>
2022-10-13 21:08:40 -0300

    feat: Modal Editar entrega

--- www/Resources/View/Modals/ModalEdit.php
+++ www/Resources/View/Modals/ModalEdit.php
@@ -10,20 +10,26 @@
         <div class="modal-content">
 
             <div class="modal-header">
-                <h5 class="modal-title">Modal title</h5>
+                <h5 class="modal-title">Editar Entrega</h5>
                 <button type="button" class="btn btn-close" data-dismiss="modal"></button>
             </div>
 
             <div class="modal-body">
-                <p>Modal body text goes here.</p>
+                <form method="POST" style="display: flex; justify-content: space-evenly;">
+                    <label>Titulo da Entrega</label>
+                    <input name="title-delivery" type="text" value="{[title-delivery}}">
             </div>
 
             <div class="modal-footer">
-                <button type="button" class="btn btn-primary">Salvar alterações</button>
-                <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cancelar</button>
+                <div class="modal-footer">
+                    <button class="btn btn-danger" type="submit" name="editButton" value="{{id}}">
+                        Editar
+                    </button>
+                    </form>
+                    <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cancelar</button>
+                </div>
             </div>
         </div>
     </div>
-</div>
-<!-- BootStrap Script -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+    <!-- BootStrap Script -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
